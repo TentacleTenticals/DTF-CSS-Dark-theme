efbeed7d08a16ee33e981b82e4e4c764f1496649
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2022-07-23 17:26:21 +0300

    Update DTF comments panel scrolling (for DTF CSS theme).js

--- DTF comments panel scrolling (for DTF CSS theme).js
+++ DTF comments panel scrolling (for DTF CSS theme).js
@@ -1,13 +1,15 @@
 // ==UserScript==
 // @name         DTF comments panel scrolling (for DTF CSS theme)
 // @namespace    https://github.com/TentacleTenticals/DTF-CSS-Dark-theme
-// @version      0.1
+// @version      1.0
 // @description  Делает возможным скролл мышью в панели комментариев, если скроллбар включён.
 // @author       Tentacle Tenticals
 // @match        https://dtf.ru/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=dtf.ru
 // @grant        none
+// @license MIT
 // ==/UserScript==
+/* jshint esversion:8 */
 
 (function() {
     'use strict';
