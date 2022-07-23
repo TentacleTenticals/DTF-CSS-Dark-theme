39890c2d682cd04debcf17ad5c831c1e65d4fc04
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2022-07-23 17:20:58 +0300

    Create DTF comments panel scrolling (for DTF CSS theme).js
    Скрипт для активации скролла в панели комментариев.

---
+++ DTF comments panel scrolling (for DTF CSS theme).js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         DTF comments panel scrolling (for DTF CSS theme)
+// @namespace    https://github.com/TentacleTenticals/DTF-CSS-Dark-theme
+// @version      0.1
+// @description  Делает возможным скролл мышью в панели комментариев, если скроллбар включён.
+// @author       Tentacle Tenticals
+// @match        https://dtf.ru/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=dtf.ru
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+window.addEventListener('wheel', commentScroll, true);
+function commentScroll(e){
+    //console.log(e.composedPath())
+    if(e.composedPath().includes(document.querySelector(`.layout__right-column > div > div > div+div > div`))){
+        if(e.deltaY >= 100){
+            document.querySelector(`.layout__right-column > div > div > div+div > div`).scrollTop += 100
+        }else
+        if(e.deltaY <= -100){
+            document.querySelector(`.layout__right-column > div > div > div+div > div`).scrollTop -= 100
+        }
+    }
+}
+
+})();
